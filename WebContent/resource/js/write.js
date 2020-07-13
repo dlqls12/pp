320810182581930431dec0c2940d4e025dec9745
dlqls12 <dlqls0190@naver.com>
2020-07-13 09:09:17 +0900

    토스트에디터 적용

--- WebContent/resource/js/write.js
+++ WebContent/resource/js/write.js
@@ -1,23 +1,24 @@
 var writeFormSubmitted = false;
 
 function submitWriteForm(form) {
-  
-  form.body.value = form.body.value.trim();
-  if ( form.body.value.length == 0 ) {
-    alert('내용을 입력해주세요.');
-    form.body.focus();
-    
-    return;
-  }
-  
-  form.title.value = form.title.value.trim();
-  if ( form.title.value.length == 0 ) {
-    alert('제목을 입력해주세요.');
-    form.title.focus();
-    
-    return;
-  }
-  
-  form.submit();
-  writeFormSubmitted = true;
+	form.title.value = form.title.value.trim();
+	if (form.title.value.length == 0) {
+		alert('제목을 입력해주세요.');
+		form.title.focus();
+
+		return;
+	}
+	
+	var source = editor1.getMarkdown().trim();
+	if (source.length == 0) {
+		alert('내용을 입력해주세요.');
+		editor1.focus();
+		return;
+	}
+	form.body.value = source;
+	form.submit();
+	
+	
+	form.submit();
+	writeFormSubmitted = true;
 }
